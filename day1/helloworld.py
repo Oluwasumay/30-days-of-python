3bf26fb2cc22d94a4831afdce53d34db6330611e
oluwaumay <sumbash06@gmail.com>
2024-09-27 12:13:05 +0100

    Feat:day 1 complete

---
+++ day1/helloworld.py
@@ -0,0 +1,18 @@
+print(3+4)       #addition
+print(3-4)       #subtraction
+print(3*4)       #multiplication
+print(3/4)       #division
+print(3**4)      #exponential
+print(3//4)      #floor dividion operator
+print(3%4)       #modulus
+print('sumayyah')    #string
+print('bashar')      #string
+print('nigeria')     #string
+print('i am enjoying 30 days of python')      #string
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['asabeneh','python','finland']))
+print(type('sumayyah'))
+print(type('bashar'))
